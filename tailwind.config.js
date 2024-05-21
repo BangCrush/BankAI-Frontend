5d814808e758dd9e85153f712fdf9cd748836f63
chaeyeon-yang <myday0827@naver.com>
2024-05-21 14:41:39 +0900

    [chore] 스토리북 디자인 시스템 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
+      minWidth: px0_1000,
+      minHeight: px0_1000,
+      maxWidth: px0_1000,
+      maxHeight: px0_1000,
+      width: px0_1000,
+      height: px0_1000,
+      borderRadius: px0_1000,
+      fontSize: px0_100,
+      lineHeight: px0_100,
+      padding: px0_1000,
+      margin: px0_1000,
       colors: {
         "main-color": "#8785F6",
         "sub-color": "#C9C8FF",
