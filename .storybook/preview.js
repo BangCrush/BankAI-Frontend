4019b3152aa7aa44d253da7c2d8363b28174bd7b
chaeyeon-yang <myday0827@naver.com>
2024-05-21 16:01:50 +0900

    [test] 스토리북 테스트

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,9 @@
+import '../src/App.css'
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
