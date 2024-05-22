34682fb87a1f02d9c59f2e1da9ddb3082adfc412
chaeyeon-yang <myday0827@naver.com>
2024-05-22 11:06:29 +0900

    [feat] 상품 정보 컴포넌트

---
+++ src/stories/molecules/product/index.stories.jsx
@@ -0,0 +1,19 @@
+import store from "../../../lib/store";
+
+import { Provider } from "react-redux";
+import Product from "./index";
+
+export default {
+  component: Product,
+  title: "molecules/product",
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+  tags: ["autodocs"],
+};
+
+export const Default = {};
+Default.args = {
+  name: "급여하나 월복리 적금",
+  promotion: "월급 받고 이자 차곡",
+  period: "1년",
+  rate: 5.85,
+};
