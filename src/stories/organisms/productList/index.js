01680d6f51c7a841809fe30adfea4166b856daf2
chaeyeon-yang <myday0827@naver.com>
2024-05-22 11:29:56 +0900

    [fix]: api 명세에 맞춰서 props전달

--- src/stories/organisms/productList/index.js
+++ src/stories/organisms/productList/index.js
@@ -1,32 +1,17 @@
 import Product from "../../molecules/product";
 
-const ProductList = () => {
+const ProductList = ({ data }) => {
   return (
-    <div className="grid gap-y-3 grid-cols-1 border border-gray-border px-20 py-30 rounded-20 ">
-      <Product
-        name={"급여하나 월복리 적급"}
-        promotion={"월급 받고 이자 차곡"}
-        period={"1년"}
-        rate={5.85}
-      />
-      <Product
-        name={"급여하나 월복리 적급"}
-        promotion={"월급 받고 이자 차곡"}
-        period={"1년"}
-        rate={5.85}
-      />
-      <Product
-        name={"급여하나 월복리 적급"}
-        promotion={"월급 받고 이자 차곡"}
-        period={"1년"}
-        rate={5.85}
-      />
-      <Product
-        name={"급여하나 월복리 적급"}
-        promotion={"월급 받고 이자 차곡"}
-        period={"1년"}
-        rate={5.85}
-      />
+    <div className="grid gap-y-10 border border-gray-300 px-20 py-30 rounded-20">
+      {data.map((product) => (
+        <Product
+          key={product.prodCode}
+          name={product.prodName}
+          promotion={product.prodPromo}
+          period={product.joinPeriod}
+          rate={product.prodRate}
+        />
+      ))}
     </div>
   );
 };
