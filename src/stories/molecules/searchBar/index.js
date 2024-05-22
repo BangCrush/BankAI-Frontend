364b7c129cb351cfe576c746cb3d6527b6ebe9d6
chaeyeon-yang <myday0827@naver.com>
2024-05-22 14:14:34 +0900

    [fix]: 불필요한 스타일 삭제

--- src/stories/molecules/searchBar/index.js
+++ src/stories/molecules/searchBar/index.js
@@ -6,8 +6,7 @@ const SearchBar = ({ placeholder }) => {
     <div className="relative w-full">
       <input
         placeholder={placeholder}
-        className="rounded-12 w-full border border-main-color shadow-custom px-14 py-13 placeholder:text-gray-placeholder pr-12"
-        style={{ paddingRight: "40px" }}
+        className="rounded-12 w-full border border-main-color shadow-custom px-14 py-13 placeholder:text-gray-900 pr-12"
       />
       <SearchIcon className="absolute right-3 top-1/2 transform -translate-y-1/2 text-main-color" />
     </div>
