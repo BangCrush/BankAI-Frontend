ebd674b193ac501e981fac3cce95df9c8343d7be
chaeyeon-yang <myday0827@naver.com>
2024-05-23 11:05:55 +0900

    [feat] 색상 클래스명 변경

--- src/stories/molecules/mainCarousel/index.js
+++ src/stories/molecules/mainCarousel/index.js
@@ -1,8 +1,8 @@
-import MediumButton from '../../atoms/mediumButton'
+import MediumButton from 'stories/atoms/mediumButton'
 
 const mainCarousel = ({data}) => {
   return (
-    <div className="border-1 border-solid border-gray-600 rounded-20 px-20 py-12 max-w-285 max-y-193">
+    <div className="border-1 border-solid border-gray-border rounded-20 px-20 py-12 max-w-285 max-y-193">
       <p className="text-black-900 text-13 font-semibold mb-10">{data.prodName}</p>
       <p className="text-gray-900 text-10 fon mb-10">{data.prodType} {data.accCode}</p>
       <p className="text-black-900 text-24 font-extrabold mb-30">{data.accBalance}</p>
