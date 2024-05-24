6edd1d226af21f9dcf3d46ef8e1243677c9e7312
chaeyeon-yang <myday0827@naver.com>
2024-05-24 16:16:48 +0900

    [fix] 자잘한 디자인 수정

--- src/stories/molecules/prodButtons/index.js
+++ src/stories/molecules/prodButtons/index.js
@@ -1,17 +1,19 @@
 import { useState } from "react";
 
 const ProdButtons = () => {
-
   let [clicked, setClicked] = useState(0);
-  let prodType = ['전체', '예금', '적금', '대출', '입출금'];
+  let prodType = ["전체", "예금", "적금", "대출", "입출금"];
 
   return (
     <div>
       {prodType.map((a, i) => (
-        <button 
-        key={i} 
-        className={`px-10 py-6 mr-10 rounded-20 text-13 ${clicked === i ? "bg-black text-white" : "bg-gray-800 text-black"}`}
-        onClick={() => {setClicked(i)}}>
+        <button
+          key={i}
+          className={`px-14 py-6 mr-10 rounded-20 text-13 ${clicked === i ? "bg-black text-white" : "bg-gray-800 text-black"}`}
+          onClick={() => {
+            setClicked(i);
+          }}
+        >
           {a}
         </button>
       ))}
@@ -20,5 +22,3 @@ const ProdButtons = () => {
 };
 
 export default ProdButtons;
-
-
