e1e8beedf53b4d5331fa9ae4d414616a746a5066
hikiman9 <skyainq@naver.com>
2024-05-24 11:13:55 +0900

    [design] 앱 뷰 -> 태블랫 뷰 중간 변경

--- src/stories/molecules/prodButtons/index.js
+++ src/stories/molecules/prodButtons/index.js
@@ -6,11 +6,11 @@ const ProdButtons = () => {
   let prodType = ['전체', '예금', '적금', '대출', '입출금'];
 
   return (
-    <div className="max-w-360">
+    <div>
       {prodType.map((a, i) => (
         <button 
         key={i} 
-        className={`px-10 py-6 mr-10 rounded-20 text-11 ${clicked === i ? "bg-black text-white" : "bg-gray-800 text-black"}`}
+        className={`px-10 py-6 mr-10 rounded-20 text-13 ${clicked === i ? "bg-black text-white" : "bg-gray-800 text-black"}`}
         onClick={() => {setClicked(i)}}>
           {a}
         </button>
