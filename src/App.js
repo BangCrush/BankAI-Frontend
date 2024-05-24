d1981dc1ab7a16f694af05b4dcdb436e3c192c80
chaeyeon-yang <myday0827@naver.com>
2024-05-24 16:17:21 +0900

    [test] 페이지 라우팅 연결 테스트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MAIN_LAYOUT_ROUTES_URL } from "./routes/mainLayoutRouter";
+import MainLayout from "stories/templates/MainLayout";
+import "App.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          {Object.values(MAIN_LAYOUT_ROUTES_URL).map((route) => {
+            return (
+              <Route
+                key={route.name}
+                path={route.path()}
+                element={<route.component />}
+              />
+            );
+          })}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
