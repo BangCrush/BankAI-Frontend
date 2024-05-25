a8b53bbe3b1dbf011ba1daf05dd36847d3cda85f
chaeyeon-yang <myday0827@naver.com>
2024-05-25 19:47:07 +0900

    [fix] api 변경에 따른 데이터 구조 변경

--- src/stories/organisms/productList/index.js
+++ src/stories/organisms/productList/index.js
@@ -1,18 +1,33 @@
-import Product from "../../molecules/product";
+import React from "react";
+import ProdType from "stories/molecules/prodType";
+import Product from "stories/molecules/product";
 
 const ProductList = ({ data }) => {
   return (
-    <div className="grid gap-y-10 border border-gray-300 px-20 py-30 rounded-20">
-      {data.map((product) => (
-        <Product
-          key={product.prodCode}
-          name={product.prodName}
-          promotion={product.prodPromo}
-          period={product.joinPeriod}
-          rate={product.prodRate}
-        />
-      ))}
-    </div>
+    <>
+      {data.map((item, itemIndex) => {
+        const prodType = Object.keys(item)[0];
+        const products = item[prodType];
+
+        return (
+          <div key={itemIndex}>
+            <ProdType prodType={prodType} prodCount={products.length} />
+            <div className="grid gap-y-10 bg-white border border-gray-300 px-20 pt-25 pb-20 rounded-20 mb-35">
+              {products.map((product, index) => (
+                <Product
+                  key={product.prodCode}
+                  name={product.prodName}
+                  promotion={product.prodPromo}
+                  period={product.joinPeriod}
+                  rate={product.prodRate}
+                  isLast={index === products.length - 1}
+                />
+              ))}
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 };
 
--- src/stories/organisms/productList/index.stories.jsx
+++ src/stories/organisms/productList/index.stories.jsx
@@ -14,20 +14,40 @@ export const Default = {};
 Default.args = {
   data: [
     {
-      prodCode: "1",
-      prodType: "1",
-      prodName: "급여하나 통장",
-      prodPromo: "월급통장 하나로 저축까지 똑똑하게",
-      joinPeriod: "1년",
-      prodRate: 5.85,
+      예금: [
+        {
+          prodCode: "1",
+          prodName: "급여하나 통장",
+          prodPromo: "월급통장 하나로 저축까지 똑똑하게",
+          joinPeriod: "1년",
+          prodRate: 5.85,
+        },
+        {
+          prodCode: "2",
+          prodName: "달달하나 통장",
+          prodPromo: "급여이체 하나만으로 우대 금리받자",
+          joinPeriod: "1년",
+          prodRate: 3.85,
+        },
+      ],
     },
     {
-      prodCode: "2",
-      prodType: "3",
-      prodName: "달달하나 통장",
-      prodPromo: "급여이체 하나만으로 우대 금리받자",
-      joinPeriod: "1년",
-      prodRate: 3.85,
+      적금: [
+        {
+          prodCode: "1",
+          prodName: "급여하나 적금통장",
+          prodPromo: "월급통장 하나로 저축까지 똑똑하게",
+          joinPeriod: "1년",
+          prodRate: 2.85,
+        },
+        {
+          prodCode: "2",
+          prodName: "달달하나 적금통장",
+          prodPromo: "급여이체 하나만으로 우대 금리받자",
+          joinPeriod: "1년",
+          prodRate: 1.85,
+        },
+      ],
     },
   ],
 };
