d263c33d30f1341cb5cdcec99e83ec4c7a36214a
chaeyeon-yang <myday0827@naver.com>
2024-05-28 09:29:07 +0900

    [feat] 배경색이 없는 레이아웃

---
+++ src/stories/templates/SubLayout.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+const SubLayout = () => {
+  return (
+    <div className="min-h-[100vh] flex flex-col">
+      <div className="max-w-[1440px] px-40 pt-30 w-full flex flex-col flex-1">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default SubLayout;
