be692bd144f8dac91a00b20615f0316beb4ff441
bigstar9906 <bigstar9906@naver.com>
2024-05-29 15:29:02 +0900

    [feat] 바텀시트  Slide up 기능 구현

--- src/stories/organisms/bottomSheet/index.js
+++ src/stories/organisms/bottomSheet/index.js
@@ -1,31 +1,35 @@
 import { Close } from "@mui/icons-material";
-import { Modal } from "@mui/material";
+import { Modal, Slide } from "@mui/material";
 import { useState } from "react";
-function BottomSheet({page}) {
-    const [open, setOpen] = useState(true);
-    const handleOpen = ()=>{setOpen(true)}
-    const handleClose = ()=>{setOpen(false)}
-    console.log(page)
-    return (
+function BottomSheet({ page }) {
+  const [open, setOpen] = useState(true);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  console.log(page);
+  return (
     <div>
-    <button onClick={handleOpen}>✔</button>
-    <Modal
+      <button onClick={handleOpen}>✔</button>
+      <Modal
         open={open}
         onClose={handleClose}
-        style={{border: 'none'}}
-        >
-        <div className="bg-white w-full border-none rounded-t-20 pb-40 absolute bottom-0 outline-none">
+        style={{ border: "none" }}
+        closeAfterTransition
+      >
+        <Slide direction="up" in={open} mountOnEnter unmountOnExit>
+          <div className="bg-white w-full border-none rounded-t-20 pb-40 absolute bottom-0 outline-none">
             <div className="flex justify-between p-20">
-                <Close onClick={()=>setOpen(false)}/>
+              <Close onClick={() => setOpen(false)} />
             </div>
-            <div className="whitespace-nowrap w-max mx-auto">
-            {page}
-            </div>
-        </div>
-    </Modal>
-    </div>  
-          
-    );
+            <div className="whitespace-nowrap w-max mx-auto">{page}</div>
+          </div>
+        </Slide>
+      </Modal>
+    </div>
+  );
 }
 
 export default BottomSheet;
