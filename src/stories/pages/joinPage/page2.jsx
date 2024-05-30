68c3a72e8b5203ab0a3b597dcaf83cb3e3bf9b78
chaeyeon-yang <myday0827@naver.com>
2024-05-30 19:43:06 +0900

    [feat] 회원가입 페이지-파트2, 파트3

--- src/stories/pages/joinPage/page2.jsx
+++ src/stories/pages/joinPage/page2.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Input from "stories/atoms/input";
 import LongButton from "stories/atoms/longButton";
 import ShortButton from "stories/atoms/shortButton";
 import Title from "stories/atoms/title";
 
 const Page2 = ({ moveNextPage, registForm, setRegistForm }) => {
-  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [addressDetail, setAddressDetail] = useState("");
 
-  const handleName = (e) => {
-    const newName = e.target.value;
-    setName(newName);
+  const handleAddressDetail = (e) => {
+    const userAddrDetail = e.target.value;
+    setAddressDetail(userAddrDetail);
     setRegistForm((draft) => {
-      draft.userName = newName;
+      draft.userAddrDetail = userAddrDetail;
     });
   };
   const onPopup = () => {
@@ -19,25 +20,46 @@ const Page2 = ({ moveNextPage, registForm, setRegistForm }) => {
     window.open(url, "_blank", "width=500, height=480");
   };
 
+  useEffect(() => {
+    const handleMessage = (event) => {
+      if (event.data.address) {
+        setAddress(event.data.address);
+        setRegistForm((draft) => {
+          draft.userAddr = event.data.userAddr;
+        });
+      }
+    };
+
+    window.addEventListener("message", handleMessage);
+
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, [setRegistForm]);
+
   return (
-    <div>
+    <>
       <Title text1={"회원가입"} text2={""} />
       <div className="pl-10">주소지를 입력해주세요.</div>
       <div className="mt-35 flex flex-col space-y-4">
         <div className="flex items-center justify-between space-x-3">
-          <Input placeholder={"우편번호"} onChange={handleName} />
+          <Input placeholder={"우편번호"} />
           <span className="mb-10" onClick={onPopup}>
             <ShortButton text={"찾기"} />
           </span>
         </div>
 
-        <Input placeholder={"주소"} onChange={handleName} />
-        <Input placeholder={"상세주소"} onChange={handleName} />
+        <Input placeholder={"주소"} value={address || ""} readonly={true} />
+        <Input placeholder={"상세주소"} onChange={handleAddressDetail} />
       </div>
       <div className="flex flex-col justify-center items-center fixed left-0 bottom-0 w-full px-40 mb-50">
-        <LongButton text={"다음"} active={!!name} onClick={moveNextPage} />
+        <LongButton
+          text={"다음"}
+          active={!!address && !!addressDetail}
+          onClick={moveNextPage}
+        />
       </div>
-    </div>
+    </>
   );
 };
 
--- src/stories/pages/joinPage/page3.jsx
+++ src/stories/pages/joinPage/page3.jsx
@@ -2,21 +2,20 @@ import useValid from "hooks/useValid";
 import React, { useState } from "react";
 import Input from "stories/atoms/input";
 import LongButton from "stories/atoms/longButton";
+import ShortButton from "stories/atoms/shortButton";
 import Title from "stories/atoms/title";
 
 const Page3 = ({ moveNextPage, registForm, setRegistForm }) => {
   // 서버통신 코드 로직
   // const { mutate: signUp } = useMutation(postSignUp);
 
-  const [name, setName] = useState("");
   const { validText, isValid } = useValid(registForm);
   const [error, setError] = useState(false);
 
-  const handleName = (e) => {
-    const newName = e.target.value;
-    setName(newName);
+  const handleChange = (field) => (e) => {
+    const value = e.target.value;
     setRegistForm((draft) => {
-      draft.userName = newName;
+      draft[field] = value;
     });
   };
 
@@ -52,12 +51,29 @@ const Page3 = ({ moveNextPage, registForm, setRegistForm }) => {
       <Title text1={"회원가입"} text2={""} />
       <div className="pl-10">아이디/패스워드를 설정해주세요.</div>
       <div className="mt-35 flex flex-col space-y-4">
-        <Input placeholder={"아이디"} onChange={handleName} />
-        <Input placeholder={"비밀번호"} onChange={handleName} />
-        <Input placeholder={"비밀번호 재확인"} onChange={handleName} />
+        <div className="flex items-center justify-between space-x-3">
+          <Input placeholder={"아이디"} onChange={handleChange("userId")} />
+          <span className="mb-10" onClick={() => {}}>
+            <ShortButton text={"중복체크"} />
+          </span>
+        </div>
+
+        <Input placeholder={"비밀번호"} onChange={handleChange("userPwd")} />
+        <Input
+          placeholder={"비밀번호 재확인"}
+          onChange={handleChange("userRePwd")}
+        />
       </div>
       <div className="flex flex-col justify-center items-center fixed left-0 bottom-0 w-full px-40 mb-50">
-        <LongButton text={"가입하기"} active={!!name} onClick={handleClick} />
+        <LongButton
+          text={"가입하기"}
+          active={
+            isValid.isUserId &&
+            isValid.isUserPassword &&
+            isValid.isUserRePassword
+          }
+          onClick={handleClick}
+        />
       </div>
     </div>
   );
