d55d535657cd79867f04e590ed380fa36e1d2268
chaeyeon-yang <myday0827@naver.com>
2024-05-30 17:01:00 +0900

    [feat] 우편번호 찾기

---
+++ src/stories/pages/zipcodePage/index.js
@@ -0,0 +1,14 @@
+import DaumPostcode from "react-daum-postcode";
+
+const ZipCodePage = () => {
+  return (
+    <div>
+      <DaumPostcode
+      // style={postCodeStyle}
+      // onComplete={onCompletePost}
+      ></DaumPostcode>
+    </div>
+  );
+};
+
+export default ZipCodePage;
