71c9269e0e2ddfef2d3e9632c47ea5f7f2764e99
chaeyeon-yang <myday0827@naver.com>
2024-05-30 20:58:04 +0900

    [fix] 스타일 수정

--- src/stories/atoms/title/index.js
+++ src/stories/atoms/title/index.js
@@ -1,6 +1,6 @@
 const Title = ({ text1, text2 }) => {
   return (
-    <div className="whitespace-pre-wrap	text-20 font-semibold p-10">
+    <div className="whitespace-pre-wrap	text-20 font-semibold p-10 mt-10">
       <div>
         {text1}
         <br />
