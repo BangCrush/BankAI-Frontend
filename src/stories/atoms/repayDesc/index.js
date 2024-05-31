dab4bd553af227557e5f04498dfb5c386a0dce7e
chaeyeon-yang <myday0827@naver.com>
2024-05-31 17:14:41 +0900

    [fix] 스타일 수정

--- src/stories/atoms/repayDesc/index.js
+++ src/stories/atoms/repayDesc/index.js
@@ -2,6 +2,7 @@ const RepayDesc = ({ text1, text2 }) => {
   return (
     <div className="bg-gray-menu rounded-12 p-20 whitespace-pre-wrap">
       {text1} <br />
+      <br />
       {text2}
     </div>
   );
