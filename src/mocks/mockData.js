4b2194a5397d3a388a103422ae041bc8dfe2766d
chaeyeon-yang <myday0827@naver.com>
2024-05-31 19:52:23 +0900

    [design] 적금가입 페이지

---
+++ src/mocks/mockData.js
@@ -0,0 +1,12 @@
+const savings = {
+  userCode: "111",
+  prodCode: "DEPOSIT",
+  amount: 100000,
+  period: "1년",
+  outAccount: "2492384239874",
+  autoTransferte: 14,
+  accountPwd: 1234,
+  accTrsfLimit: 300000,
+};
+
+export { savings };
--- src/stories/molecules/mediumInput/index.js
+++ src/stories/molecules/mediumInput/index.js
@@ -1,12 +1,17 @@
-const MediumInput = ({ placeholder, active, text }) => {
+const MediumInput = ({ placeholder, active, text, value, onChange, error }) => {
   return (
-    <div className="flex items-center">
-      <input
-        placeholder={placeholder}
-        readOnly={active ? false : true}
-        className={`rounded-10 bg-gray-input px-12 py-15 max-w-190 placeholder:font-semibold ${active ? "placeholder:text-gray" : "placeholder:text-main-color"} text-12 mr-10`}
-      />
-      <div className="text-14 text-black font-semibold">{text}</div>
+    <div className="flex flex-col">
+      <div className="flex items-center pt-10 mb-10">
+        <input
+          placeholder={placeholder}
+          readOnly={active ? false : true}
+          value={value ? "" : value}
+          className={`rounded-10 outline-none bg-gray-input px-12 py-15 max-w-190 placeholder:font-semibold ${active ? "placeholder:text-gray" : "placeholder:text-main-color"} text-12 mr-10`}
+          onChange={onChange}
+        />
+        <div className="text-14 text-black font-semibold">{text}</div>
+      </div>
+      {error && <div className="text-13 text-err-color px-5">{error}</div>}
     </div>
   );
 };
