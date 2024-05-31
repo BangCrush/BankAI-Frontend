4bee2b7b7d750996e2992f3e1b598c0ec61d6b55
hikiman9 <skyainq@naver.com>
2024-05-31 14:29:10 +0900

    [feat] 상품 상세 페이지 내에 들어갈 컴포넌트

---
+++ src/stories/organisms/prodBenefit/index.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+const ProdBenefit = ({ data }) => {
+
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    const value = parseInt(e.target.value, 10);
+    if (isNaN(value)) {
+        setInputValue("");
+        return;
+    }
+    const rate = (data.prodRate * 10) / 1000;
+    let finalAmount;
+
+    if (data.prodAcc === "SIMPLE") {
+      finalAmount = value * (1 + rate);
+    } else if (data.prodAcc === "COMPOUND") {
+      finalAmount = value * Math.pow((1 + rate / 12), 12);
+    }
+
+    setInputValue(Number(finalAmount.toFixed()).toLocaleString());
+
+  };
+
+  return (
+    <div className="p-40">
+      <div className="py-17">
+        <p className="text-16 font-extrabold">나의 예상 혜택</p>
+        <div className="my-20 py-16 px-10 bg-gray-200 rounded-12">
+          <div className="p-10 grid grid-cols-2">
+            <div className="text-14 text-gray-950">만기금액</div>
+            <div className="text-right text-17 font-extrabold here">
+              {inputValue}원
+            </div>
+          </div>
+          <div className="p-10 grid grid-cols-2">
+            <div className="text-14 text-gray-950">적용금리</div>
+            <div className="text-right text-17 font-extrabold">
+              {data.prodRate}%
+            </div>
+          </div>
+        </div>
+        <p className="mb-20 text-16 font-extrabold">
+          한 달에{" "}
+          <input
+            type="text"
+            placeholder="1,000 "
+            className="w-90 text-right placeholder:italic placeholder:text-right focus:outline-none"
+            onChange={handleInputChange}
+          />
+          {data.prodType === "SAVINGS" ? "원 씩" : "원을"}
+        </p>
+        <p className="text-16 font-extrabold">
+          <span className="text-main-color">12개월</span> 동안 저축하기
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProdBenefit;
--- src/stories/organisms/prodBenefit/index.stories.jsx
+++ src/stories/organisms/prodBenefit/index.stories.jsx
@@ -1,11 +1,11 @@
 import store from "libs/store";
 
 import { Provider } from "react-redux";
-import ProdDetail from ".";
+import ProdBenefit from ".";
 
 export default {
-  component: ProdDetail,
-  title: "page/prodDetail",
+  component: ProdBenefit,
+  title: "organisms/ProdBenefit",
   decorators: [(story) => <Provider store={store}>{story()}</Provider>],
   tags: ["autodocs"],
 };
@@ -14,11 +14,11 @@ export const Default = {};
 Default.args = {
   data: {
     prodCode: 200202,
-    prodType: "DEPOSIT",
+    prodType: "SAVINGS",
     prodName: "BKS 내맘대로 적금",
     prodDesc: "저축 금액을 내 마음대로 디자인하는 적금 상품입니다.",
     joinPeriod: 12,
-    prodRate: 4.0,
+    prodRate: 4,
     prodMin: 10000,
     prodMax: 3000000,
     joinMember: "실명의 개인 또는 개인사업자",
