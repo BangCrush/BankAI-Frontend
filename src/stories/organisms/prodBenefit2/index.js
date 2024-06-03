92725b7070e0bd986301779d8efde396d6d25410
chaeyeon-yang <myday0827@naver.com>
2024-06-03 17:06:25 +0900

    [feat] 가입 상품에 따른 상세 컴포넌트 변경

--- src/stories/organisms/prodBenefit2/index.js
+++ src/stories/organisms/prodBenefit2/index.js
@@ -1,19 +1,22 @@
 const ProdBenefit2 = () => {
   return (
     <div className="p-40 relative">
-      <p className="text-22 font-extrabold mt-17 mb-24">
-        거래 쌓고 혜택도 쌓고
-      </p>
-      <p className="text-16 font-semibold mb-24">
+      <p className="text-22 font-bold mt-17 mb-24">거래 쌓고 혜택도 쌓고</p>
+      <p className="text-16 font-medium mb-24">
         주거래 이체하고 수수료 우대받아 보세요.
       </p>
-      <p className="text-16 font-semibold mb-24">
-        2024년 12월 31일까지 가입한<br/> 
-        선착순 30만명에게<br/>
-        가입 후 1년 동안<br/>
-        연 1.0%의 특별 우대금리 제공
+      <p className="text-16 font-medium mb-24">
+        2024년 12월 31일까지 가입한
+        <br />
+        선착순 30만명에게
+        <br />
+        가입 후 1년 동안
+        <br />연 1.0%의 특별 우대금리 제공
       </p>
-      <img src="/assets/inoutacc1.svg" className="absolute w-200 bottom-10 right-10"/>
+      <img
+        src="/assets/inoutacc1.svg"
+        className="absolute w-280 bottom-10 right-10"
+      />
     </div>
   );
 };
---
+++ src/stories/organisms/prodBenefit3/index.js
@@ -0,0 +1,19 @@
+const ProdBenefit3 = () => {
+  return (
+    <div className="p-40 relative">
+      <p className="text-22 font-bold mt-17 mb-24">집에서도 편하게 대출!</p>
+      <p className="text-16 font-medium mb-24">온라인 대출을 통해</p>
+      <p className="text-16 font-medium mb-24">
+        은행에 가지 않고도
+        <br />
+        편리하게 온라인으로 가입 가능 ✨
+      </p>
+      <img
+        src="/assets/loan1.svg"
+        className="absolute w-280 h-220 bottom-10 right-10"
+      />
+    </div>
+  );
+};
+
+export default ProdBenefit3;
