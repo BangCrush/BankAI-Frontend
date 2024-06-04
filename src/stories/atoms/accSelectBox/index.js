e056c833276411b002f8d60c81a58191fdc2bf3a
chaeyeon-yang <myday0827@naver.com>
2024-06-04 13:19:42 +0900

    [feat] 내 계좌 목록 조회 및 선택

--- src/stories/atoms/accSelectBox/index.js
+++ src/stories/atoms/accSelectBox/index.js
@@ -1,11 +1,14 @@
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import { accList } from "mocks/mockData";
 import { useState } from "react";
 
-const AccSelectBox = ({ text, selectedOption, setSelectedOption }) => {
+const AccSelectBox = ({
+  text,
+  options = [],
+  selectedOption,
+  setSelectedOption,
+}) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const options = accList;
   const handleToggle = () => {
     setIsOpen((prev) => !prev);
   };
@@ -17,7 +20,7 @@ const AccSelectBox = ({ text, selectedOption, setSelectedOption }) => {
 
   const renderSelectedOption = () => {
     if (selectedOption) {
-      return `${selectedOption.accCode}`;
+      return `${selectedOption}`;
     }
     return "옵션을 선택하세요";
   };
@@ -40,7 +43,7 @@ const AccSelectBox = ({ text, selectedOption, setSelectedOption }) => {
           <KeyboardArrowDownIcon className="text-superSubColor" />
         </div>
 
-        {isOpen && (
+        {isOpen && options.length > 0 && (
           <div className="absolute top-full left-0 z-10 w-full rounded-12 border mt-1 bg-white p-3 gray-border">
             {options.map((item, index) => (
               <div
@@ -48,7 +51,7 @@ const AccSelectBox = ({ text, selectedOption, setSelectedOption }) => {
                 className="px-10 py-10 cursor-pointer hover:bg-gray-200 bg-white text-15"
                 onClick={() => handleOptionSelect(item)}
               >
-                {item.accCode}
+                {item}
               </div>
             ))}
           </div>
