a9ced66fb53b76f0941b523306bae9f047d62467
chaeyeon-yang <myday0827@naver.com>
2024-06-05 19:04:38 +0900

    [feat] 인기상품 자동플레이 케러셀

--- src/stories/organisms/popularProd/index.js
+++ src/stories/organisms/popularProd/index.js
@@ -1,17 +1,39 @@
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import Slider from "react-slick";
 
-const PopularProd = ({ promotion, img }) => {
+const PopularProd = ({ promotion, imgs, data }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 200,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  };
+
   return (
-    <div className="flex justify-between border border-gray-border rounded-20 shadow-custom p-25 bg-white">
-      <div className="flex flex-col">
-        <div className="text-20 font-semibold mb-10">{promotion}</div>
-        <div className="flex items-center text-gray-900 text-13">
-          확인하기
-          <ArrowForwardIosIcon sx={{ fontSize: 14 }}></ArrowForwardIosIcon>
+    <Slider key={"key"} className="shadow-custom rounded-20" {...settings}>
+      {data.map((item, index) => (
+        <div
+          key={index}
+          className="border border-gray-border rounded-20 px-30 py-35 bg-white "
+        >
+          <div className="flex justify-between">
+            <div className="flex flex-col ">
+              <div className="text-20 font-semibold mb-10">
+                {item.prodPromo}
+              </div>
+              <div className="flex items-center text-gray-900 text-13">
+                확인하기
+                <ArrowForwardIosIcon sx={{ fontSize: 14 }} />
+              </div>
+            </div>
+            <img className="w-60" src={`/assets/${imgs[index]}.png`} />
+          </div>
         </div>
-      </div>
-      <img src="/assets/popular1.svg" />
-    </div>
+      ))}
+    </Slider>
   );
 };
 
