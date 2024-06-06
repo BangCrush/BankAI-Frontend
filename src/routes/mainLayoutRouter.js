6a3fdedf0bcbff1f4a234d3433eb80ba31c057cc
bigstar9906 <bigstar9906@naver.com>
2024-06-06 12:13:33 +0900

    [chore] 인덱스 페이지 front 페이지로 라우팅

--- src/routes/mainLayoutRouter.js
+++ src/routes/mainLayoutRouter.js
@@ -1,6 +1,12 @@
+import { redirect } from "react-router-dom";
 import MainPage from "stories/pages/mainPage";
 
 export const MAIN_LAYOUT_ROUTES_URL = {
+  index:{
+    name: "index",
+    path: () => "/",
+    component: ()=>{window.location.href = "/front"},
+  },
   mainPage: {
     name: "메인 페이지",
     path: () => "/main",
