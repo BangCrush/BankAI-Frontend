8fd4e0ca65cbc9b0e81e20102fac9d5010e74a80
bigstar9906 <bigstar9906@naver.com>
2024-06-06 12:09:56 +0900

    [chore] password 페이지 popup 위치 수정

--- src/constants/password.js
+++ src/constants/password.js
@@ -1,1 +1,1 @@
-export const PwdWindowOptions = "toolbar=no,scrollbars=no,resizable=no,status=no,menubar=no,width=400, height=540, top=330,left=100"
+export const PwdWindowOptions = "toolbar=no,scrollbars=no,resizable=no,status=no,menubar=no,width=400, height=540, top=330,left=200"
