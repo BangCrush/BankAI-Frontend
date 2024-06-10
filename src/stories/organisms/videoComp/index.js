9ca8e3f8510f9629aea457152e70686882a5d845
LimEunSang <dmstkd2905@naver.com>
2024-06-10 14:27:59 +0900

    [fix] merge 과정에서 생긴 중복 코드 제거

--- src/stories/organisms/videoComp/index.js
+++ src/stories/organisms/videoComp/index.js
@@ -50,19 +50,6 @@ function VideoComp({
     }
   }, [autoPlay]);
 
-  // 동영상 시간 업데이트 함수
-  const addTimeUpdate = () => {
-    const observedVideoElement = ref && ref.current;
-    if (observedVideoElement) {
-      observedVideoElement.addEventListener("timeupdate", function () {
-        setCurrentTime(observedVideoElement.currentTime);
-      });
-      // 컴포넌트가 처음 마운트 될 때 동영상 시작 할지 말지 여부 (여기서는 시작되게 했음)
-      setIsVideoPlaying(1);
-      observedVideoElement.play();
-    }
-  };
-
   useEffect(() => {
     if (src) {
       addTimeUpdate();
